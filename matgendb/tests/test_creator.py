f42d469c0d822a8a8382fff18a5173ac78771460
Shyue Ping Ong <shyue@mit.edu>
2013-02-27 16:17:43 -0500

    More unittests. Improved mgdb query.

--- matgendb/tests/test_creator.py
+++ matgendb/tests/test_creator.py
@@ -34,13 +34,13 @@ class VaspToDbTaskDroneTest(unittest.TestCase):
         all_paths = []
         for path in os.walk(os.path.join(test_dir, 'db_test')):
             all_paths.extend(self.drone.get_valid_paths(path))
-        self.assertEqual(len(all_paths), 3)
+        self.assertEqual(len(all_paths), 5)
 
     def test_assimilate(self):
         queen = BorgQueen(self.drone)
         queen.serial_assimilate(os.path.join(test_dir, 'db_test'))
         data = queen.get_data()
-        self.assertEqual(len(data), 3)
+        self.assertEqual(len(data), 5)
         for d in data:
             dir_name = d['dir_name']
             if dir_name.endswith("killed_mp_aflow"):
@@ -50,11 +50,23 @@ class VaspToDbTaskDroneTest(unittest.TestCase):
             elif dir_name.endswith("stopped_mp_aflow"):
                 self.assertEqual(d['state'], "stopped")
                 self.assertEqual(d['pretty_formula'], "ThFe5P3")
-            else:
+            elif dir_name.endswith("sucess_mp_aflow"):
                 self.assertEqual(d['state'], "successful")
                 self.assertEqual(d['pretty_formula'], "TbZn(BO2)5")
                 self.assertAlmostEqual(d['output']['final_energy'],
                                        -526.66747274)
+            elif dir_name.endswith("Li2O_aflow"):
+                self.assertEqual(d['state'], "successful")
+                self.assertEqual(d['pretty_formula'], "Li2O")
+                self.assertAlmostEqual(d['output']['final_energy'],
+                                       -14.31446494)
+                self.assertEqual(len(d["calculations"]), 2)
+            elif dir_name.endswith("Li2O"):
+                self.assertEqual(d['state'], "successful")
+                self.assertEqual(d['pretty_formula'], "Li2O")
+                self.assertAlmostEqual(d['output']['final_energy'],
+                                       -14.31337758)
+                self.assertEqual(len(d["calculations"]), 1)
 
 if __name__ == "__main__":
     unittest.main()
