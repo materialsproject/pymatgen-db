ee5753d08b0ee4a47143de6964bae13c2252ceab
Shyue Ping Ong <shyuep@gmail.com>
2013-03-02 22:50:33 -0500

    Improve handling of different criteria strings.

--- matgendb/webui/home/views.py
+++ matgendb/webui/home/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 
 import json
+import re
 
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -30,24 +31,24 @@ def index(request):
     return render_to_response("home/templates/index.html",
                               RequestContext(request, d))
 
+
 @csrf_exempt
 def query(request):
     if request.method == 'POST':
         try:
-            critstr = request.POST["criteria"]
-            try:
-                criteria = {"pretty_formula": Composition(critstr).reduced_formula}
-            except:
-                try:
-                    criteria = {"task_id": int(critstr)}
-                except ValueError:
-                    try:
-                        syms = [Element(sym).symbol
-                                for sym in critstr.split("-")]
-                        syms.sort()
-                        criteria = {"chemsys": "-".join(syms)}
-                    except:
-                        criteria = json.loads(critstr)
+            critstr = request.POST["criteria"].strip()
+            if re.match("^[\w\(\)]+$", critstr):
+                criteria = {"pretty_formula":
+                                Composition(critstr).reduced_formula}
+            elif re.match("^\d+$", critstr):
+                criteria = {"task_id": int(critstr)}
+            elif re.match("^[A-Za-z\-]+$", critstr):
+                syms = [Element(sym).symbol
+                        for sym in critstr.split("-")]
+                syms.sort()
+                criteria = {"chemsys": "-".join(syms)}
+            else:
+                criteria = json.loads(critstr)
             properties = request.POST["properties"].split()
             if not properties:
                 properties = None
