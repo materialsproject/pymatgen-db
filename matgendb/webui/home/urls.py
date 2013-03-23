efd7f30ed93694ae50c24998e1e77b0c5242656e
Shyue Ping Ong <shyuep@gmail.com>
2013-03-23 07:55:07 -0400

    Remove deprecated call.

--- matgendb/webui/home/urls.py
+++ matgendb/webui/home/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('matgendb.webui.home.views',
-    # regex are retarded and python is stupid
-    # thank you for those words of wisdom, Michael
     (r'^$', 'index'),
     (r'^query$', 'query')
 )
