6b608f8da53887f02fdcfdd8bff2af860852e594
Shyue Ping Ong <shyuep@gmail.com>
2013-03-29 13:33:00 -0400

    Udpate version numbers.

--- matgendb/__init__.py
+++ matgendb/__init__.py
@@ -9,7 +9,7 @@ pymatgen objects for analysis purposes.
 
 __author__ = "Shyue Ping Ong"
 __date__ = "Mar 1 2013"
-__version__ = "0.3.1"
+__version__ = "0.3.2b"
 
 
 from query_engine import QueryEngine
