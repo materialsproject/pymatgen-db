f3d750a0b0b4a64ba0677a5c1226bc55b45c196e
Dan Gunter <dkgunter@lbl.gov>
2014-04-07 04:59:03 -0700

    include collection keys in template

--- matgendb/webui/home/views.py
+++ matgendb/webui/home/views.py
@@ -7,6 +7,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from matgendb.query_engine import QueryEngine
+from matgendb import util as dbutil
 
 
 qe = None
@@ -14,9 +15,11 @@ qe = None
 mgdb_config = os.environ.get("MGDB_CONFIG", "")
 if mgdb_config:
     config = json.loads(mgdb_config)
+    if not dbutil.normalize_auth(config, readonly_first=True):
+        config["user"] = config["password"] = None
     qe = QueryEngine(host=config["host"], port=config["port"],
-                     database=config["database"], user=config["readonly_user"],
-                     password=config["readonly_password"],
+                     database=config["database"], user=config["user"],
+                     password=config["password"],
                      collection=config["collection"],
                      aliases_config=config.get("aliases_config", None))
 
@@ -25,5 +28,8 @@ def index(request):
         return "<p>No database configured.</p>"
     d = config.copy()
     d["ndocs"] = qe.collection.count()
+    d["keys"] = dbutil.collection_keys(qe.collection)
     return render_to_response("home/templates/index.html",
                               RequestContext(request, d))
+
+
