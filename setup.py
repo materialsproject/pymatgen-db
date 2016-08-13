ca1044877f91c0ae4658dfe5df5c7daa4778b90d
__sp__ <ongsp@eng.ucsd.edu>
2016-08-13 14:42:57 -0700

    proper update to setup.py

--- setup.py
+++ setup.py
@@ -9,28 +9,14 @@ from setuptools import setup, find_packages
 
 with open("README.rst") as f:
     long_desc = f.read()
-with open("VERSION") as f:
-    version = f.read().strip()
-
-static_data = []
-for parent, dirs, files in os.walk(os.path.join("matgendb", "webui",
-                                                "static")):
-    for f in files:
-        if not f.endswith(".psd"):
-            print("Adding {} to static_data".format(f))
-            static_data.append(os.path.join(parent.lstrip("matgendb/webui/"),
-                                            f))
 
 setup(
     name="pymatgen-db",
     packages=find_packages(),
     version="0.6.1",
-    install_requires=["pymatgen>=3.2.3", "monty>=0.7.0",
-                      "pymongo>=2.8", "prettytable",
-                      "django", "mongomock", "smoqe", "enum34"],
-    package_data={"matgendb": ["*.json"],
-                  "matgendb.webui.home": ["templates/*"],
-                  "matgendb.webui": static_data},
+    install_requires=["pymatgen>=3.2.3", "monty>=0.9.0",
+                      "pymongo>=2.8", "prettytable", "mongomock", "smoqe", "enum34"],
+    package_data={"matgendb": ["*.json"]},
     author="Shyue Ping Ong, Dan Gunter",
     author_email="shyuep@gmail.com",
     maintainer="Dan Gunter",
@@ -43,7 +29,10 @@ setup(
     keywords=["vasp", "gaussian", "materials", "project", "electronic",
               "structure", "mongo"],
     classifiers=[
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
@@ -52,8 +41,7 @@ setup(
         "Topic :: Scientific/Engineering :: Physics",
         "Topic :: Scientific/Engineering :: Chemistry",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Database",
-        "Topic :: Database :: Front-Ends"
+        "Topic :: Database"
     ],
     scripts=[os.path.join("scripts", f) for f in os.listdir("scripts")
              if not os.path.isdir(os.path.join("scripts", f))]
