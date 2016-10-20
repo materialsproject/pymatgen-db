21fd7391d79648d1f43d79cdd46120ab53b97d79
__sp__ <ongsp@eng.ucsd.edu>
2016-10-20 15:21:10 -0700

    Remove use of prettytable.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     version="0.6.3",
     install_requires=["pymatgen>=4.4.1", "monty>=0.9.6",
-                      "pymongo>=2.8", "prettytable", "mongomock", "smoqe",
+                      "pymongo>=2.8", "mongomock", "smoqe",
                       "enum34"],
     package_data={"matgendb": ["*.json"]},
     author="Shyue Ping Ong, Dan Gunter",
