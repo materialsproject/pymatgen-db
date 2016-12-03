33362e72ec683b57d955aa1663152542f1c32a35
__sp__ <ongsp@eng.ucsd.edu>
2016-12-03 11:18:57 -0800

    Fix mode.

--- matgendb/tests/test_dbconfig.py
+++ matgendb/tests/test_dbconfig.py
@@ -82,7 +82,7 @@ class SettingsTestCase(unittest.TestCase):
         """
         # sending file obj, or its path, should have same effect
         tmp = "dbconfigtest.json"
-        with open(tmp, "w") as f:
+        with open(tmp, "r+") as f:
             json.dump(self.cfg, f)
             f.flush()
             # reset file to beginning
