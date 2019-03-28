c86d4454fd903562e9f7bd98a48c5708c34065b0
__sp__ <shyuep@gmail.com>
2019-03-28 16:56:59 -0700

    Update setup.py.

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
